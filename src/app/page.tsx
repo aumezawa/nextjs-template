00aaafed1f1e258b86354aeec3183e747383f692
aumezawa <letzt.apostel@gmail.com>
2025-01-11 17:07:20 +0900

    v1.0.0: Initial version

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+export default function Home() {
+  return (
+    <>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="text-3xl">
+          Hello World!
+        </div>
+      </div>
+    </>
+  );
+}
